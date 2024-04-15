df8f016b09add783d288230cc5f6b26dfea8f024
atalhaaydogan <atalhaaydogan@gmail.com>
2024-04-15 21:23:55 +0300

    EfCore context oluşturma

---
+++ MovieApp.Web/Data/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Web.Entity;
+
+namespace MovieApp.Web.Data
+{
+    public class MovieContext: DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
